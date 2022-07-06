be99747df61daee098d097b27626da17b093ab5e
andrey rodriguez <desarrolloweb6@tae-ltda.com>
2022-07-05 19:38:10 -0500

    proyecto con inst bases

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <Text>
+        <Icon name="add-outline" size={30} color="#900" />;
+        </Text>
+    </View>
+  )
+}
